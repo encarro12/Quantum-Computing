7862628e53ec03592d6dd4bb037446444e468b23
encarro12 <ecarrogarrido12@gmail.com>
2023-02-19 13:49:56 +0100

    Update tilings.cpp
    Change recursive function to a backtracking algorithm

--- Tests/Wang-Tiles/tilings.cpp
+++ Tests/Wang-Tiles/tilings.cpp
@@ -3,53 +3,55 @@
 using namespace std;
 
 const int M = 2;
-const int N = 3;
+const int N = 4;
 
 using tiling = vector<vector<tile>>;
 
 /*
 */
-vector<tiling> & operator+=(std::vector<tiling>& A, const std::vector<tiling>& B)
-{
-	A.reserve(A.size() + B.size());
-	A.insert(A.end(), B.begin(), B.end());
-	return A;
-}
-
-/*
-*/
-vector<tiling> tilings_recursive(tiling& R, const tiles& T_row,
+void tilings_backtracking(vector<tiling> & thao, tiling & R, const tiles& T_row,
 	const tiles& T_other, int row, int column) {
-	vector<tiling> thao;
-	//
-	if (row == M)
-		thao.push_back(R);
-	else if (column == N) {
-		thao += tilings_recursive(R, T_row, T_other, row + 1, 0);
-		thao += tilings_recursive(R, T_other, T_row, row + 1, 0);
-	}
-	else {
-		vector<tile> valid = T_row.get_valid_tiles(R[row][column]);
-		for (tile t : valid) {
-			R[row][column] = t;
-			// New boundary conditions in right and lower edge
+	vector<tile> valid = T_row.get_valid_tiles(R[row][column]);
+	for (tile t : valid) {
+		R[row][column] = t;
+		if (row == M - 1 && column == N - 1) // It is a valid tiling
+			thao.push_back(R);
+		else {
 			if (row + 1 < M)
 				R[row + 1][column].a = t.c;
-			if (column + 1 < N)
+			if (column == N - 1) {
+				tilings_backtracking(thao, R, T_other, T_row, row + 1, 0);
+				tilings_backtracking(thao, R, T_row, T_other, row + 1, 0);
+			}
+			else {
 				R[row][column + 1].b = t.d;
-			thao += tilings_recursive(R, T_row, T_other, row, column + 1);
+				tilings_backtracking(thao, R, T_row, T_other, row, column + 1);
+			}
 		}
 	}
-	return thao;
 }
 
 /*
 */
-vector<tiling> tilings(tiling& R, const tiles& T2_map, const tiles& T2_3_map) {
-	vector<tiling> thao;
-	thao += tilings_recursive(R, T2_map, T2_3_map, 0, 0);
-	thao += tilings_recursive(R, T2_3_map, T2_map, 0, 0);
-	return thao;
+void tilings(vector<tiling> & thao, tiling& R, const tiles& T2_map, const tiles& T2_3_map) {
+	tilings_backtracking(thao, R, T2_map, T2_3_map, 0, 0);
+	tilings_backtracking(thao, R, T2_3_map, T2_map, 0, 0);
+}
+
+/*
+* To analyze the stability of a boundary condition, this is the 
+only function that has to be modified.
+*/
+tiling boundary_conditions() {
+	tiling R;
+	for (int row = 0; row < M; row++) {
+		vector<tile> tmp;
+		for (int column = 0; column < N; column++) {
+			tmp.push_back({ INT_MAX, INT_MAX, INT_MAX, INT_MAX });
+		}
+		R.push_back(tmp);
+	}
+	return R;
 }
 
 
@@ -57,7 +59,6 @@ int main() {
 	/*
 	*
 	*/
-	int a = 0;
 	tiles T2_map = tiles({
 		{1,-1,2,-1},
 		{1,-1,1,0},
@@ -81,5 +82,15 @@ int main() {
 		});
 	/*
 	*/
+
+	/*
+	*/
+	tiling R = boundary_conditions();
+
+	/*
+	*/
+	vector<tiling> thao;
+	tilings(thao, R, T2_map, T2_3_map);
+
 	return 0;
 }
