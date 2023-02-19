3eaa16c356345930619e193209029e90f1c25f60
encarro12 <ecarrogarrido12@gmail.com>
2023-02-19 17:46:43 +0100

    Update tilings.cpp
    Added distance between boundary conditions

--- Tests/Wang-Tiles/tilings.cpp
+++ Tests/Wang-Tiles/tilings.cpp
@@ -51,17 +51,16 @@ void boundary_conditions(tiling & R, int option) {
 		}
 		R.push_back(tmp);
 	}
-	// If option == 1, then fixed boundary condition
+	// If option == 1, then fixed boundary condition for thao
 	if(option == 1) {
 		// Upper edge
 		R[0][0].a = 1;
-		R[0][1].a = 0; /**/
+		R[0][1].a = 1; /**/
 		R[0][2].a = 1;
 		R[0][3].a = 1;
 		R[0][4].a = 1;
 		R[0][5].a = 1;
 
-		/*
 		// Left edge
 		R[0][0].b = -1;
 		R[1][0].b = -1.0 / 3;
@@ -69,6 +68,7 @@ void boundary_conditions(tiling & R, int option) {
 		R[3][0].b = -1;
 		R[4][0].b = 0;
 
+		/*
 		// Lower edge
 		R[4][0].c = 1;
 		R[4][1].c = 0;
@@ -85,6 +85,61 @@ void boundary_conditions(tiling & R, int option) {
 		R[4][5].d = 2.0/3;
 		*/
 	}
+	// If option == 1, then fixed boundary condition for gamma
+	else if (option == 2) {
+		// Upper edge
+		R[0][0].a = 1;
+		R[0][1].a = 1; /**/
+		R[0][2].a = 1;
+		R[0][3].a = 0;
+		R[0][4].a = 1;
+		R[0][5].a = 1;
+
+		// Left edge
+		R[0][0].b = -1;
+		R[1][0].b = -1.0 / 3;
+		R[2][0].b = 0;
+		R[3][0].b = -1;
+		R[4][0].b = 0;
+
+		/*
+		// Lower edge
+		R[4][0].c = 1;
+		R[4][1].c = 0;
+		R[4][2].c = 1;
+		R[4][3].c = 2;
+		R[4][4].c = 1;
+		R[4][5].c = 1;
+
+		// Right edge
+		R[0][5].d = -1;
+		R[1][5].d = 2.0/3;
+		R[2][5].d = -1.0/3;
+		R[3][5].d = -1;
+		R[4][5].d = 2.0/3;
+		*/
+	}
+}
+
+/*
+*/
+double d(const tiling& R, const tiling& Q) {
+	double count = 0;
+	for (int row = 0; row < M; row++)
+		for (int column = 0; column < N; column++)
+			if (!(R[row][column] == Q[row][column]))
+				count++;
+	return count / (N * M);
+}
+
+/*
+*/
+double d(const vector<tiling>& thao, const vector<tiling>& gamma) {
+	double distance = INT_MAX;
+	for (tiling R : thao)
+		for (tiling Q : gamma)
+			distance = min(distance, d(R, Q));
+	return distance;
 }
 
 
@@ -118,13 +173,16 @@ int main() {
 
 	/*
 	*/
-	tiling R;
+	tiling R, Q;
 	boundary_conditions(R, 1);
+	boundary_conditions(Q, 2);
 
 	/*
 	*/
-	vector<tiling> thao;
+	vector<tiling> thao, gamma;
 	tilings(thao, R, T2_map, T2_3_map);
+	tilings(gamma, Q, T2_map, T2_3_map);
 
+	cout << "d(gamma, thao) = " << d(gamma, thao) << endl;
 	return 0;
 }
